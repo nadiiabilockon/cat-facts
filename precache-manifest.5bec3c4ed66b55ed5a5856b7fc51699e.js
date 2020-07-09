887a703e2a9a3ef52ccbe7ceaf5422ed560fd1ad
nadiiabilockon <11071995nad@gmail.com>
2020-07-10 00:13:18 +0300

    Updates

---
+++ precache-manifest.5bec3c4ed66b55ed5a5856b7fc51699e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267cc68855e98bac3445ee37bb8a1ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c24618611cec4d12730",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0adf079a00987234b36a",
+    "url": "/static/css/main.7d707dd9.chunk.css"
+  },
+  {
+    "revision": "6c24618611cec4d12730",
+    "url": "/static/js/2.24cd84a0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.24cd84a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adf079a00987234b36a",
+    "url": "/static/js/main.12afcf4d.chunk.js"
+  },
+  {
+    "revision": "3b45fad25118713073e7",
+    "url": "/static/js/runtime-main.f985c4da.js"
+  }
+]);
